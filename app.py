0ae99d1b4ed0966f475defe3c23c9ecef58c92b5
Vijayakanth M <vikymahendiran123@gmail.com>
2025-03-20 14:17:36 +0530

    port error fixed

--- app.py
+++ app.py
@@ -160,4 +160,6 @@ def index():
     return render_template("index.html")
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.getenv("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=True)
+
