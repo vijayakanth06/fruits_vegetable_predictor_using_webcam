c2ee614ea925578670a694c93481faf1b2a99c79
Vijayakanth M <vikymahendiran123@gmail.com>
2025-03-20 15:08:18 +0530

    onrender

--- app.py
+++ app.py
@@ -161,5 +161,5 @@ def index():
 
 if __name__ == "__main__":
     port = int(os.getenv("PORT", 5000))
-    app.run(host="0.0.0.0", port=port, debug=True)
+    app.run(host="0.0.0.0", port=port, debug=False)
 
